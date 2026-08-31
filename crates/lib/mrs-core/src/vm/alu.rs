5f734387ed4e0ba156b3f5b853b4264b79087426
on9au <151502370+on9au@users.noreply.github.com>
2026-08-31 15:38:39 +1000

    tests: add ALU tests

--- crates/lib/mrs-core/src/vm/alu.rs
+++ crates/lib/mrs-core/src/vm/alu.rs
@@ -16,3 +16,22 @@ impl Alu {
         x.wrapping_sub(y)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(Alu::add(5, 3), 8);
+        assert_eq!(Alu::add(-5, -3), -8);
+        assert_eq!(Alu::add(-5, 3), -2);
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(Alu::sub(5, 3), 2);
+        assert_eq!(Alu::sub(-5, -3), -2);
+        assert_eq!(Alu::sub(-5, 3), -8);
+    }
+}
